1c2cd3ed46ad33226e17acfe6b0a149c52893433
wqk <zgahwuqiankun@qq.com>
2017-12-12 20:36:59 +0800

    约会网站匹对改进第一次提交。明天加注释

---
+++ kNN.py
@@ -0,0 +1,78 @@
+"""
+Created on Dec 12, 2017
+kNN: k Nearest Neighbors
+@author: Ezra
+@email:zgahwuqiankun@qq.com
+约会网站匹对改进
+"""
+
+from numpy import *
+import operator
+from os import listdir
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+#归一化
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.50      #hold out 10%
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+    print(errorCount)
+
+def classifyPerson():
+    resultList = ['一点都不会','很小的可能','很大的可能']
+    percentTats = float(input("打电子游戏的时间所占百分比："))
+    ffMiles = float(input("每年飞行里程数（mile）："))
+    iceCream = float(input("每周吃下的冰激凌公升数："))
+    datingDataMat, datingLabels = file2matrix("./dataSet/datingTestSet2.txt")
+    normMat, ranges,minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges,normMat,datingLabels,3)
+    print("你会喜欢这个人的概率是：",resultList[classifierResult - 1])
+
+classifyPerson()
---
+++ test.py
@@ -0,0 +1,16 @@
+import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+#datingDataMat, datingLabels = kNN.file2matrix("./dataSet/datingTestSet2.txt")
+
+#通过画布把数据集里面的点画出来
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*np.array(datingLabels),15.0*np.array(datingLabels))#?函数还没整明白，回头看
+# ax.set_xlabel('time of playing game ')
+# ax.set_ylabel('number of ice-cream consumed per week ')
+# plt.show()
+
+
